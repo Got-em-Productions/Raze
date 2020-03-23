bfe5c88f85208510190b16bc6839c28fb20239c5
Will Magee <wmagee03@gmail.com>
2020-03-23 18:38:07 -0400

    Created script for breaking blocks (currently destroys any type

---
+++ Assets/DestoryBlocks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryBlocks : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col) {
+        Debug.Log(col);
+        if (col.gameObject.tag == "Block") {
+            Destroy(col.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
